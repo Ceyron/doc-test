ff52b08863334b41af1333c9fe27f583225ec656
Felix Köhler <27728103+Ceyron@users.noreply.github.com>
2024-06-21 08:58:38 +0200

    adapt style of docstring

--- doc_test/_hello_world_printer.py
+++ doc_test/_hello_world_printer.py
@@ -2,11 +2,13 @@ def print_hello_world(name: str, *, additional_text: str = ""):
     """
     Top level function to print hello world message.
 
-    *Arguments*:
-        - name: Name of the person to greet.
-        - additional_text: Additional text to append to the message.
+    **Arguments:**
 
-    *Returns*:
-        - None
+    - name: Name of the person to greet.
+    - additional_text: Additional text to append to the message.
+
+    **Returns:**
+
+    - None
     """
     print(f"Hello, {name}! {additional_text}")
